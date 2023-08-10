9d1ce8690e9faae129fc43a9840a7b38ef0b1074
marcelogmoura <mgmoura@gmail.com>
2023-08-10 17:35:10 -0300

    first commit

---
+++ src/main/java/com/mgmoura/ApiProdutosredditApplication.java
@@ -0,0 +1,13 @@
+package com.mgmoura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProdutosredditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProdutosredditApplication.class, args);
+	}
+
+}
