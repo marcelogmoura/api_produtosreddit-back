68ef80506cc067db9d1fe7a0663540da895a40c7
marcelogmoura <mgmoura@gmail.com>
2023-08-21 19:40:59 -0300

    classes de teste implementadas e pow atualizado

--- src/test/java/com/mgmoura/ApiProdutosredditApplicationTests.java
+++ src/test/java/com/mgmoura/ApiProdutosredditApplicationTests.java
@@ -1,13 +1,49 @@
 package com.mgmoura;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ApiProdutosredditApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@Order(1)
+	public void testProdutoPost() {
+		fail("Não implementado!");
 	}
+
+	@Test
+	@Order(2)
+	public void testProdutoPut() {
+		fail("Não implementado!");
+	}
+	
+	@Test
+	@Order(3)
+	public void testProdutoGetAll() {
+		fail("Não implementado!");
+	}
+
+	@Test
+	@Order(4)
+	public void testProdutoGetById() {
+		fail("Não implementado!");
+	}
+
+
+	@Test
+	@Order(5)
+	public void testProdutoDelete() {
+		fail("Não implementado!");
+	}
+	
+	
+
 
 }
